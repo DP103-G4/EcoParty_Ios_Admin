289c96401f0a88b7974ace027ee12c6ffbf4d3fb
diehawk951 <diehawk951@gmail.com>
2020-02-18 19:00:22 +0800

    Initial Commit

---
+++ EcoParty_Ios_Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoParty_Ios_Admin
+//
+//  Created by 梁嘉峻 on 2020/2/18.
+//  Copyright © 2020 梁嘉峻. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
