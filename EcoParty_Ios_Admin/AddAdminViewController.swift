b0808d5ed9a5fa49fe0dbe8a4b23ec0b3c3ae229
mac <meteoricdust1701@hotmail.com>
2020-02-18 20:27:18 +0800

    0218 UI

---
+++ EcoParty_Ios_Admin/AddAdminViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddAdminViewController.swift
+//  EcoParty_Ios_Admin
+//
+//  Created by mac on 2020/2/18.
+//  Copyright © 2020 梁嘉峻. All rights reserved.
+//
+
+import UIKit
+
+class AddAdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
